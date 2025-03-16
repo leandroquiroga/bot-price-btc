da822e03e836132c7226fd263ae303c1797de714
leandroquiroga <leandroquiroga9514@gmail.com>
2025-03-16 20:33:36 -0300

    Se agrega nuevo comando de alerta y monitoreo de precio

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,10 +1,13 @@
 import { startCommand } from "./start.js";
 import { priceCommand } from './price.js';
 import { clearCommand } from './clear.js';
+import { alertCommand, checkAlerts } from "./alert.js";
 
 
 export const commands = {
     start: startCommand,
     price: priceCommand,
+    alert: alertCommand,
     clear: clearCommand,
+    checkAlerts: checkAlerts
 };
