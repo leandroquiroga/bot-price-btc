37d2b9adf1943bbde8ce5cacca3ea10ebfeb9b82
leandroquiroga <leandroquiroga9514@gmail.com>
2025-03-19 16:51:44 -0300

    Se mejora funcionalidades dandole mejor legibilidad y mejor estructura del codigo

--- src/events/events.js
+++ src/events/events.js
@@ -1,20 +1,26 @@
+import { handlePriceResponse } from "../commands/price.js";
 import { validCommands } from "../config/configuration.js";
 import { errorMessage, notCommandMessage, welcomeMessage } from "../utils/messages.js";
 import { sendMessage } from "../utils/telegram.js";
 
-export const eventsMessage = async (bot, msg, messageIds) => {
+const validText = (text) => text && text.startsWith('/');
+const command = (text) => text.slice(1).split(' ')[0].split('@')[0].toLowerCase();
+const isUserAndValidText = (text, awaitingPriceResponse, chatId) => text && awaitingPriceResponse.has(chatId);
+ 
+export const eventsMessage = async (bot, msg, messageIds, awaitingPriceResponse) => {
   const chatId = msg.chat.id;
   const text = msg.text;
-
   try {
-    
-    if (text && text.startsWith('/')) {
-      const command = text.slice(1).split(' ')[0].split('@')[0].toLowerCase();
-      if (!validCommands.includes(command)) {
+
+    if (validText(text)) {
+      if (!validCommands.includes(command(text))) {
         const errMsg = errorMessage(welcomeMessage)
         await sendMessage(bot, chatId, errMsg, messageIds);
       }
-    } else if (text) {
+    } else if (isUserAndValidText(text, awaitingPriceResponse, chatId)) {
+      await handlePriceResponse(bot, msg, text, messageIds);
+      awaitingPriceResponse.delete(chatId)
+    } else {
       const commandInvalid = notCommandMessage(welcomeMessage)
       await sendMessage(bot, chatId, commandInvalid, messageIds);
     }
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,14 +1,36 @@
 import axios from "axios";
 import { enviroment } from "../config/enviroment.js";
+import { sendMessage } from "./telegram.js";
+import { buildUrl } from "./function.js";
 
-export const getBitcoinPrice = async () => { 
+const cryptoIds = {
+  BTC: 'bitcoin',
+  ETH: 'ethereum',
+  SOL: 'solana',
+};
+
+export const getPrice = async (bot, chatId, messageIds, crypto) => { 
   try {
-    const url = `${enviroment.URL_BASE}${enviroment.URL_BASE_PATH}`;
-    const response = await axios.get(url);
+    const cleanCrypto = crypto.replace('/', '').toUpperCase();
+
+    const cryptoId = cryptoIds[cleanCrypto];
+
+    if (!cryptoId) {
+      console.error('Cripto inválida ', crypto);
+      sendMessage(bot, chatId, 'Cripto inválida ', messageIds);
+      return null;
+    };
+
+    const url = `${enviroment.URL_BASE}`;
+    const path = `${enviroment.URL_BASE_PATH}`;
+    const urlComplete = buildUrl(url, path, { fsym: cleanCrypto, tsyms: 'USDT' });
+
+    const response = await axios.get(urlComplete);
     const price = response.data.USDT;
     return price;
   } catch (error) {
-    console.error('Error al obtener el precio de Bitcoin:', error);
+    console.error('Error al obtener el precio de Bitcoin', error);
+    sendMessage(bot, chatId, 'Error al obtener el precio de Bitcoin', messageIds);
     return null
   }
 };
